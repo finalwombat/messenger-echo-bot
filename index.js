8a9ca294c94334f17ab8260e35314d29dde487a5
Jay Kelly <comital333@gmail.com>
2017-04-20 12:02:36 +1000

    sendGenericMessage function

--- index.js
+++ index.js
@@ -85,7 +85,50 @@ function recievedMessage(event){
 }
 
 function sendGenericMessage(recipientID, messageText){
+  const messageData = {
+    recipient: {
+      id: recipientID
+    },
+    message: {
+      attchment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  }
 
+  callSendAPI(messageData)
 }
 
 function sendTextMessage(recipientID, messageText) {
