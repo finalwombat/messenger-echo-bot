ed10139190bc2cf91e65c7353aa2545f828ee5c8
Jay Kelly <comital333@gmail.com>
2017-04-20 11:43:18 +1000

    added ! marks so I can see my logs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 // Facebook verification
 app.get('/webhook', (req, res) =>{
-  console.log('get');
+  console.log('get!!!!!!!!!!!!!!!!111');
   if(req.query['hub.verify_token'] === 'aPasswordThatWillBeSet'){
     res.status(200).send(req.query['hub.challenge'])
   }
@@ -31,7 +31,7 @@ app.get('/webhook', (req, res) =>{
 })
 
 app.post('/webhook', (req, res) => {
-  console.log('post')
+  console.log('post!!!!!!!!!!!!!!!!!1')
   var data = req.body
 
   // Make sure this is a page subscription
